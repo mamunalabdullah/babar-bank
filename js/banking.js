b5c4656cda4c640cf86a16b3927e79e522424cf7
mamunalabdullah <abdullahmamun1597@gmail.com>
2022-02-18 10:36:08 +0600

    update

--- js/banking.js
+++ js/banking.js
@@ -60,3 +60,58 @@
 //     };
 // })
 
+
+// New js code practice
+
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const valueInText = inputField.value;
+    const value = Number(valueInText);
+    inputField.value = "";
+    return value;
+}
+
+function getInnerTextValue(fieldId){
+    const tagField = document.getElementById(fieldId);
+    const valueInText = tagField.innerText;
+    const value = Number(valueInText);
+    return value;
+}
+
+function updateTotal(fieldId, amount){
+    
+    const previousTotal = getInnerTextValue(fieldId);
+    const newTotal = previousTotal + amount;
+    document.getElementById(fieldId).innerText = newTotal;
+}
+
+function updateBalance(amount, isAdding){
+
+    const previousBalance = getInnerTextValue("balance-total");
+    let newBalance;
+    if (isAdding == true) {
+        newBalance = previousBalance + amount;
+    } else {
+        newBalance = previousBalance - amount;
+    }
+    document.getElementById("balance-total").innerText = newBalance;
+}
+
+document.getElementById("deposit-btn").addEventListener("click", function(){
+    const depositMoney = getInputValue("deposit");
+    
+    if (depositMoney > 0) {
+        updateTotal("deposit-total", depositMoney);
+        updateBalance(depositMoney, true);
+    }
+})
+
+
+document.getElementById("withdraw-btn").addEventListener("click", function(){
+    const withdrawMoney = getInputValue("withdraw");
+    const balance = getInnerTextValue("balance-total")
+    if (withdrawMoney > 0 && withdrawMoney <= balance) {
+        updateTotal("withdraw-total", withdrawMoney);
+        updateBalance(withdrawMoney, false);
+    }
+})
