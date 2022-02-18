108ae969598db9512fdb2c5d58a19973938a1dc9
mamunalabdullah <abdullahmamun1597@gmail.com>
2022-02-18 08:22:47 +0600

    js code comment

--- js/banking.js
+++ js/banking.js
@@ -1,61 +1,62 @@
-function getInputValue(inputId){
-    const inputField = document.getElementById(inputId);
-    const inputMoneyText = inputField.value;
-    const inputMoney = parseFloat(inputMoneyText);
+// function getInputValue(inputId){
+//     const inputField = document.getElementById(inputId);
+//     const inputMoneyText = inputField.value;
+//     const inputMoney = parseFloat(inputMoneyText);
 
-    //clear input field
-    inputField.value = "";
-    return inputMoney;
-}
+//     //clear input field
+//     inputField.value = "";
+//     return inputMoney;
+// }
 
-function displayTotalBalance(balanceId, money){
-    const displayTotal = document.getElementById(balanceId);
-    const previousTotal = parseFloat(displayTotal.innerText);
-    const updateTotal = previousTotal + money;
-    displayTotal.innerText = updateTotal;
-}
+// function displayTotalBalance(balanceId, money){
+//     const displayTotal = document.getElementById(balanceId);
+//     const previousTotal = parseFloat(displayTotal.innerText);
+//     const updateTotal = previousTotal + money;
+//     displayTotal.innerText = updateTotal;
+// }
 
-function getCurrentBalance(){
-    const balanceTotal = document.getElementById("balance-total");
-    const oldBalance = parseFloat(balanceTotal.innerText);
-    return oldBalance;
-}
+// function getCurrentBalance(){
+//     const balanceTotal = document.getElementById("balance-total");
+//     const oldBalance = parseFloat(balanceTotal.innerText);
+//     return oldBalance;
+// }
 
-function updateBalance(depositMoney, isAdd){
-    const balanceTotal = document.getElementById("balance-total");
-    const oldBalance = getCurrentBalance();
-    if (isAdd == true) {
-        const newBalance = oldBalance + depositMoney;
-        balanceTotal.innerText = newBalance;
-    } else {
-        const newBalance = oldBalance - depositMoney;
-        balanceTotal.innerText = newBalance;
-    }
-}
+// function updateBalance(depositMoney, isAdd){
+//     const balanceTotal = document.getElementById("balance-total");
+//     const oldBalance = getCurrentBalance();
+//     if (isAdd == true) {
+//         const newBalance = oldBalance + depositMoney;
+//         balanceTotal.innerText = newBalance;
+//     } else {
+//         const newBalance = oldBalance - depositMoney;
+//         balanceTotal.innerText = newBalance;
+//     }
+// }
 
 
-// deposit button event handle
-document.getElementById("deposit-btn").addEventListener("click", function(){
-    const depositMoney = getInputValue("deposit");
+// // deposit button event handle
+// document.getElementById("deposit-btn").addEventListener("click", function(){
+//     const depositMoney = getInputValue("deposit");
     
-    if(depositMoney > 0){
-        // deposit sheet
-    displayTotalBalance("deposit-total", depositMoney);
+//     if(depositMoney > 0){
+//         // deposit sheet
+//     displayTotalBalance("deposit-total", depositMoney);
 
-    //update balance sheet
-    updateBalance(depositMoney, true);
-    };
-})
+//     //update balance sheet
+//     updateBalance(depositMoney, true);
+//     };
+// })
 
-// withdraw button event handle
-document.getElementById("withdraw-btn").addEventListener("click", function(){
-    const withdrawMoney = getInputValue("withdraw");
-    const currentBalance = getCurrentBalance();
-    if(withdrawMoney > 0 && withdrawMoney <= currentBalance){
-        // withdraw sheet
-        displayTotalBalance("withdraw-total", withdrawMoney);
+// // withdraw button event handle
+// document.getElementById("withdraw-btn").addEventListener("click", function(){
+//     const withdrawMoney = getInputValue("withdraw");
+//     const currentBalance = getCurrentBalance();
+//     if(withdrawMoney > 0 && withdrawMoney <= currentBalance){
+//         // withdraw sheet
+//         displayTotalBalance("withdraw-total", withdrawMoney);
 
-        //update balance sheet
-        updateBalance(withdrawMoney, false);
-    };
-})
+//         //update balance sheet
+//         updateBalance(withdrawMoney, false);
+//     };
+// })
+
